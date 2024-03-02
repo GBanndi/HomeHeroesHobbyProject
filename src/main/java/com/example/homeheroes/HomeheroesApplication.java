04ea60c0033671fdbf59e850f5306cae283c3a9d
GBandi <bandi293@gmail.com>
2024-03-02 11:20:22 +0100

    Init Project Backend

---
+++ src/main/java/com/example/homeheroes/HomeheroesApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeheroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeheroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homeheroes/HomeheroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeheroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
